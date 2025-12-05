fa0c668945c77ae46eb4c2ddd5b047b5d11794b2
jlkwl <24039614@myrp.edu.sg>
2025-12-05 21:28:36 +0800

    debug

--- SupermarketApp/models/Feedback.js
+++ SupermarketApp/models/Feedback.js
@@ -1,33 +1,33 @@
 const db = require('../db');
 
 const Feedback = {
-    create: (data, callback) => {
-        const sql = `
-            INSERT INTO feedback (user_id, subject, message)
-            VALUES (?, ?, ?)
-        `;
-        db.query(sql, [data.user_id, data.subject, data.message], callback);
-    },
-
-    getAll: (callback) => {
-        const sql = `
-            SELECT f.*, u.username, u.email
-            FROM feedback f
-            JOIN users u ON f.user_id = u.id
-            ORDER BY f.created_at DESC
-        `;
+    getAll(callback) {
+        const sql = `SELECT * FROM feedback ORDER BY created_at DESC`;
         db.query(sql, callback);
     },
 
-    delete: (id, callback) => {
+    getById(id, callback) {
+        const sql = `SELECT * FROM feedback WHERE id = ?`;
+        db.query(sql, [id], callback);
+    },
+
+    create(user_id, username, email, address, subject, message, callback) {
+        const sql = `
+            INSERT INTO feedback (user_id, username, email, address, subject, message)
+            VALUES (?, ?, ?, ?, ?, ?)
+        `;
+        db.query(sql, [user_id, username, email, address, subject, message], callback);
+    },
+
+    delete(id, callback) {
         const sql = `DELETE FROM feedback WHERE id = ?`;
         db.query(sql, [id], callback);
     },
 
-    reply: (id, reply, callback) => {
+    reply(id, reply, callback) {
         const sql = `
             UPDATE feedback 
-            SET admin_reply = ? 
+            SET admin_reply = ?, reply_date = NOW()
             WHERE id = ?
         `;
         db.query(sql, [reply, id], callback);
